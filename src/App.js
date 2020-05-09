61ec084729b614ccc7302859e2e372e9af033477
sang <sangphan005@gmail.com>
2020-05-09 21:08:42 +0700

    update CartItem.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Header from './components/Header';
 import ProductsContainer from './containers/ProductsContainer';
 import MessageContainer from './containers/MessageContainer';
 import CartContainer from './containers/CartContainer';
-import Footer from './components/Footer';
 
-class App extends Component {
-    render() {
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Header />
+//             <main id="mainContainer">
+//                 <div className="container">
+//                     <ProductsContainer />
+//                     <MessageContainer />
+//                     <CartContainer />
+//                 </div>
+//             </main>
+//             <h1>Well come to Hook </h1>
+//             <Footer />
+//         </div>
+//         );
+//     }
+// }
+ function App() {
+
         return (
             <div>
                 <Header />
@@ -17,10 +34,8 @@ class App extends Component {
                     <CartContainer />
                 </div>
             </main>
-            <Footer />
         </div>
         );
-    }
 }
 
 export default App;
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,73 +1,87 @@
 import React, { Component } from 'react';
 import * as Message from '../constants/Message';
 
-class CartItem extends Component {
-    render() {
-        var { item } = this.props;
-        return (
-            <tr>
-                <th scope="row">
-                    <img src={item.product.image}
-                        alt={item.product.image} className="img-fluid z-depth-0" />
-                </th>
-                <td>
-                    <h5>
-                        <strong>{item.product.name}</strong>
-                    </h5>
-                </td>
-                <td>{item.product.price}$</td>
-                <td className="center-on-small-only">
-                    <span className="qty">{item.quantity}</span>
-                    <div className="btn-group radio-group" data-toggle="buttons">
-                        <label 
-                            className="btn btn-sm btn-primary
-                            btn-rounded waves-effect waves-light"
-                            onClick={() => this.onUpdateCartItem(item.product, item.quantity - 1)}
-                        >
-                            <a href="#">—</a>
-                        </label>
-                        <label 
-                            className="btn btn-sm btn-primary
-                            btn-rounded waves-effect waves-light"
-                            onClick={() => this.onUpdateCartItem(item.product, item.quantity + 1)}
-                        >
-                            <a href="#">+</a>
-                        </label>
-                    </div>
-                </td>
-                <td>{this.showTotal(item.product.price, item.quantity)}$</td>
-                <td>
-                    <button
-                        type="button"
-                        className="btn btn-sm btn-primary waves-effect waves-light"
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title=""
-                        data-original-title="Remove item"
-                        onClick={()=> this.onDelete(item.product)}
-                    >
-                        X
-                    </button>
-                </td>
-            </tr>
-        );
-    }
+import React, { useState, useEffect } from 'react';
 
-    showTotal = (price, quantity) => {
-        return price*quantity;
-    }
-
-    onDelete = product => {
-        this.props.onDeleteProductInCart(product);
-        this.props.onChangeMessage(Message.MSG_DELETE_PRODUCT_IN_CART_SUCCESS);
-    }
-
-    onUpdateCartItem = (product, quantity) => {
-        if(quantity > 0){     
-            this.props.onUpdateCartItem(product, quantity);
-            this.props.onChangeMessage(Message.MSG_UPDATE_CART_SUCCESS);
+const CartItem = ({item, onUpdateCartItem}) => {
+// function CartItem(props) {
+    // var { item } = props;
+    const [numb, setNumb] = useState(0);
+    const [total, setTotal] = useState(0);
+    useEffect(() => {
+        // setNumb(numb - 1);
+        // setTotal((numb -1) * 500);
+    });
+    const onUpdateItem = (condition) => {
+        if(numb > 0) {
+            if(condition == 'minus') {
+                setNumb(numb - 1);
+            } else {
+                setNumb(numb + 1);
+            }
+        } else if(numb == 0 && condition == 'plus') {
+            setNumb(numb + 1);
         }
     }
+    React.useEffect(() => {
+        setTotal(numb * 500);
+    }, [numb]);
+
+    return (
+        <tr>
+            <th scope="row">
+                <img src={item.product.image}
+                    alt={item.product.image} className="img-fluid z-depth-0" />
+            </th>
+            <td>
+                <h5>
+                    <strong>{item.product.name}</strong>
+                </h5>
+            </td>
+            <td>{item.product.price}$</td>
+            <td className="center-on-small-only">
+                <span className="qty">{numb}</span>
+                <div className="btn-group radio-group" data-toggle="buttons">
+                    <label
+                        className="btn btn-sm btn-primary
+                            btn-rounded waves-effect waves-light"
+                        onClick={() => {
+                                onUpdateItem('minus')
+                            }
+                        }
+                    >
+                        <a href="#">—</a>
+                    </label>
+                    <label
+                        className="btn btn-sm btn-primary
+                            btn-rounded waves-effect waves-light"
+                        onClick={() => {
+                            onUpdateItem('plus')
+                        }}
+                    >
+                        <a href="#">+</a>
+                    </label>
+                </div>
+            </td>
+            <td>{total}$</td>
+            <td>
+                <button
+                    type="button"
+                    className="btn btn-sm btn-primary waves-effect waves-light"
+                    data-toggle="tooltip"
+                    data-placement="top"
+                    title=""
+                    data-original-title="Remove item"
+                    // onClick={() => onDelete(item.product)}
+                >
+                    X
+                    </button>
+            </td>
+        </tr>
+    );
 }
 
 export default CartItem;
+
+
+
