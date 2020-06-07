cff5a0db4a9055c118e6f5e942b2021f43384528
tmtam612 <tmtam612@gmail.com>
2020-06-07 22:36:26 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import Header from './components/Header';
 import ProductsContainer from './containers/ProductsContainer';
-import MessageContainer from './containers/MessageContainer';
-import CartContainer from './containers/CartContainer';
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {getListProducts} from './actions/index';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header />
-            <main id="mainContainer">
-                <div className="container">
-                    <ProductsContainer />
-                    <MessageContainer />
-                    <CartContainer />
-                </div>
-            </main>
+function App() {
+    const dispatch = useDispatch();
+    const productStore = useSelector(state => state.products);
+    const [products, setProducts] = useState([]);
+	const getProducts = useCallback(
+		(data) => dispatch(getListProducts(data)),
+		[dispatch],
+	);
+    React.useEffect(() => {
+		axios.get("http://127.0.0.1:8000/api/products/")
+			.then((response) => { 
+				getProducts(response.data);
+			}).catch((err) => console.log('err', err));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    React.useEffect(() => {
+		if (!productStore.products) {
+			return;
+		}
+
+		setProducts(productStore.products);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [productStore]);
+
+    return (
+        <div>
+            {
+                products && products.length > 0 &&
+                <>
+                    <Header />
+                    <main id="mainContainer">
+                        <div className="container">
+                            <ProductsContainer props={products}/>
+                        </div>
+                    </main>
+                </>
+            }
         </div>
-        );
-    }
+    );
+    
 }
 
 export default App;
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,41 +1,16 @@
-import React, { useCallback, useState } from 'react';
-import axios from "axios";
-import Products from '../components/Products';
+import React, {  useState } from 'react';
 import Product from "../components/Product";
-import {useDispatch, useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
 import {addToCart, changeMessage } from '../actions/index';
-import {getListProducts} from '../actions/index';
 
-function ProductsContainer() {
+
+function ProductsContainer({props}) {
 	const dispatch = useDispatch();
-
-	const productStore = useSelector(state => state.products);
-	const [products, setProducts] = useState([]);
-	const getProducts = useCallback(
-		(data) => dispatch(getListProducts(data)),
-		[dispatch],
-	);
-
-	React.useEffect(() => {
-		axios.get("http://127.0.0.1:8000/api/products/")
-			.then((response) => { 
-				getProducts(response.data);
-			}).catch((err) => console.log('err', err));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-
-	React.useEffect(() => {
-		if (!productStore.products) {
-			return;
-		}
-
-		setProducts(productStore.products);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [productStore]);
-
-
+	const [products] = useState(props);
 	return (
-		<Products>
+		<section className="section">
+            <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
+            <div className="row">
 			{products.map((product) => (
 				<Product
 					key={product.id}
@@ -44,7 +19,8 @@ function ProductsContainer() {
 					onChangeMessage={ message => dispatch(changeMessage(message))}
 				/>
 			))}
-		</Products>
+            </div >
+        </section>
 	);
 }
 
